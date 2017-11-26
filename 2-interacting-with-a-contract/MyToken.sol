4c787190f2686a8f39faf7db555df507817844bc
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2017-11-25 21:00:39 -0300

    Update MyToken contract

--- 2-interacting-with-a-contract/MyToken.sol
+++ 2-interacting-with-a-contract/MyToken.sol
@@ -6,17 +6,17 @@ contract MyToken {
   uint256 public constant decimals = 18;
   uint256 public constant INITIAL_SUPPLY = 10000;
 
-  address creator;
-  uint256 totalSupply;
-  mapping (address => uint256) balances;
+  address public creator;
+  uint256 public totalSupply;
+  mapping (address => uint256) public balances;
 
-  function MyToken() {
+  function MyToken() public {
     creator = msg.sender;
     totalSupply = INITIAL_SUPPLY;
     balances[creator] = INITIAL_SUPPLY;
   }
 
-  function sendTokens(address receiver, uint256 amount) returns (bool) {
+  function sendTokens(address receiver, uint256 amount) public returns (bool) {
     address owner = msg.sender;
 
     require(amount > 0);
@@ -27,7 +27,7 @@ contract MyToken {
     return true;
   }
 
-  function balanceOf(address owner) constant returns (uint256) {
+  function balanceOf(address owner) public constant returns (uint256) {
     return balances[owner];
   }
 }
